931f2a88b148d3862cf3c05e5e83c913de0939fc
hubaibp <27hubaib@gmail.com>
2025-07-17 11:37:50 +0530

    home page added

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from appp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.Home.as_view())
 ]

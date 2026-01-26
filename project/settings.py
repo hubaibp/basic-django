cc63941a6a33d48ba6231a4aa7c7ad2219594509
Hubaib P <84179982+hubaibp@users.noreply.github.com>
2026-01-26 20:51:57 +0530

    aws

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ex5m6p@(_c0a0yadcbs8k^$0h6a)^*)ppmh(ox1cb!nx+d(nx(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.53.130.180']
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
